214819d3f3ef2f779b029b11da6dc776f697ca93
Your Name <you@example.com>
2026-01-13 18:59:07 +0530

    first commit

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,20 @@
 public class Triangle {
-    //Add your programs here
+    public static void main(String args[]){
+          System.out.print("*");
+          System.out.println();
+          for(int i=0;i<3;i++){
+                 System.out.print("*");
+                 if(i<2)
+                    System.out.print(" ");
+
+}
+  System.out.println();
+  for(int i=0;i<5;i++){
+                 System.out.print("*");
+                 if(i<4)
+                    System.out.print(" ");
+
+}
+
+}
 }
